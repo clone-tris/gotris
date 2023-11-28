072dbddc271a715d4e48670551863262fc48fb8f
Abderrahmane TAHRI JOUTI <atahrijouti@paypal.com>
2023-11-28 16:14:58 +0100

    config

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"gotris/screens/game/components"
+	"gotris/config"
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	vector.StrokeLine(screen, 10, 10, 300, 100, 1, color.RGBA{0x39, 0x89, 0xd2, 0xff}, true)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return config.CANVAS_WIDTH, config.CANVAS_HEIGHT
+}
+
 func main() {
-	s := components.Square{Row: 1, Column: 1}
-	fmt.Printf("%+v\n", s)
+	game := &Game{}
+	ebiten.SetWindowSize(config.CANVAS_WIDTH, config.CANVAS_HEIGHT)
+	ebiten.SetWindowTitle("Gotris")
+
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
