445dd6b7d738ca7696466f25af056e0adbcfd6ba
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2026-09-12 19:13:18 +0200

    full drawSquare

--- painter.go
+++ painter.go
@@ -30,4 +30,34 @@ func drawGuide(canvas *ebiten.Image, x int, y int, width int, height int) {
 
 func drawSquare(canvas *ebiten.Image, x int, y int, color color.RGBA) {
 	vector.FillRect(canvas, float32(x), float32(y), float32(SW), float32(SW), color, false)
+	// left
+	fillPolygon(canvas, []point{{x, y}, {x + BW, y + BW}, {x + BW, y + SW - BW}, {x, y + SW}}, SQUARE_BORDER_SIDE)
+	// right
+	fillPolygon(canvas, []point{{x + SW, y}, {x + SW - BW, y + BW}, {x + SW - BW, y + SW - BW}, {x + SW, y + SW}}, SQUARE_BORDER_SIDE)
+	// top
+	fillPolygon(canvas, []point{{x, y}, {x + BW, y + BW}, {x + SW - BW, y + BW}, {x + SW, y}}, SQUARE_BORDER_TOP)
+	// bottom
+	fillPolygon(canvas, []point{{x, y + SW}, {x + BW, y + SW - BW}, {x + SW - BW, y + SW - BW}, {x + SW, y + SW}}, SQUARE_BORDER_BOTTOM)
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func fillPolygon(canvas *ebiten.Image, points []point, clr color.RGBA) {
+	if len(points) == 0 {
+		return
+	}
+
+	var path vector.Path
+	path.MoveTo(float32(points[0].x), float32(points[0].y))
+	for _, p := range points[1:] {
+		path.LineTo(float32(p.x), float32(p.y))
+	}
+	path.Close()
+
+	var cs ebiten.ColorScale
+	cs.ScaleWithColor(clr)
+	vector.FillPath(canvas, &path, nil, &vector.DrawPathOptions{ColorScale: cs})
 }
